7f042b2895f1cfab4ae05e7eec5c1bbfcfa7b2f9
pritisharma1511 <ps0130815@gmail.com>
2025-09-23 22:57:35 +0530

    hash function added

--- node_js_first/crypto.js
+++ node_js_first/crypto.js
@@ -3,3 +3,7 @@ const crypto = require('crypto');
 // 1. Generate a random byte sequence
 const randomValue = crypto.randomBytes(16);
 console.log('Random Bytes:', randomValue.toString('hex'));
+
+// 2. Create a SHA-256 hash of a string
+const hash = crypto.createHash('sha256').update('Hello, World!').digest('hex');
+console.log('SHA-256 Hash:', hash);
