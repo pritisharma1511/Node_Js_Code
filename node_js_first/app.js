993a61c3fa7f2571686e496865e65256134f1c85
pritisharma1511 <ps0130815@gmail.com>
2025-09-23 19:32:24 +0530

    done

--- node_js_first/app.js
+++ node_js_first/app.js
@@ -10,8 +10,16 @@
 // console.log(process);
 
 const {add ,mul,div,subs} = require('./math');
-
 console.log(add(5,10));
 console.log(mul(5,10));
 console.log(div(5,10));
 console.log(subs(5,10));
+
+
+
+// const math = require('./math');
+
+// console.log(math.add(5,10));
+// console.log(math.mul(5,10));
+// console.log(math.div(5,10));
+// console.log(math.subs(5,10));
