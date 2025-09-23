cb6b020f6ef2f3d0af338ca791ea6142f852a5b1
pritisharma1511 <ps0130815@gmail.com>
2025-09-23 22:24:32 +0530

    path_module added

--- node_js_first/path_module.js
+++ node_js_first/path_module.js
@@ -5,3 +5,12 @@ console.log(__filename);
 
 const filePath = path.join("folder", "students", "data.txt");
 console.log(filePath);
+
+const parseData = path.parse(filePath);
+const base = path.basename(filePath);
+const resolvedPath = path.resolve( filePath);
+const extname = path.extname(filePath);
+const dirname= path.dirname(filePath);
+
+
+console.log({parseData, base, resolvedPath, extname, dirname});
