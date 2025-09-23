5294f7b74adb95df768eede309b8b2480d3a49fe
pritisharma1511 <ps0130815@gmail.com>
2025-09-23 19:21:09 +0530

    done

--- node_js_first/math.js
+++ node_js_first/math.js
@@ -2,4 +2,16 @@ const add = (a,b) => {
     return a + b;
 };
 
+const subs = (a,b) => {
+    return a - b;
+};
+
+const mul = (a,b) => {
+    return a * b;
+};
+
+const div = (a,b) => {
+    return a / b;
+};
+
 module.exports = add;
