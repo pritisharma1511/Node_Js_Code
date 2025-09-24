a9fd8cbe1edd371540a73f9704374de6aea6ad43
pritisharma1511 <ps0130815@gmail.com>
2025-09-24 18:34:41 +0530

    done

--- fs/fsAsyncAwait.js
+++ fs/fsAsyncAwait.js
@@ -1,5 +1,23 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 
 const fileName = "fsAsyncAwait.txt";
 const filePath = path.join(__dirname, fileName);
+const filePath1 = __dirname;
+
+// fs.promises
+//     .readdir(filePath1)
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+
+// const readFolder = async () => {
+//     try {
+//         const res = await fs.promises.readdir(filePath1);  
+//        console.log(res);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+// readFolder();
+
+const 
