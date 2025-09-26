b053263ea95b008c9da1cf16d4f3428c6c07019f
pritisharma1511 <ps0130815@gmail.com>
2025-09-26 09:05:16 +0530

    task added

---
+++ events/task.js
@@ -0,0 +1,14 @@
+//? Challenge: "Event Maestro: Handle It All!"
+
+//! Objective
+//* Create a program using Node.js EventEmitter that:
+
+//? Listens for multiple types of user events (e.g., login, logout, purchase, and profile update).
+//? Tracks how many times each event is emitted.
+//? Logs a summary of all event occurrences when a special summary event is triggered.
+
+//! Requirements
+//? Create at least four custom events (e.g., user-login, user-logout, user-purchase, profile-update).
+//? Emit these events multiple times with different arguments (e.g., username, item purchased).
+//? Track and store the count of each event type.
+//? Define a summary event that logs a detailed report of how many times each event was triggered.
