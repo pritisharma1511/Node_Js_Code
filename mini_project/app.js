662628d7b9ffb91a818ccb4f3cd7350d18219406
pritisharma1511 <ps0130815@gmail.com>
2025-09-28 09:50:04 +0530

    todo app

--- mini_project/app.js
+++ mini_project/app.js
@@ -14,6 +14,31 @@ const showMenu = () => {
     rl.question("Choose an option :",handleInput)
 }
 
-
+const handleInput = (option) => {
+    if(option === 1){
+        readline.question("enter the Task:", (task) =>{
+            todos.push(task);
+            console.log("Task added:" ,task);
+            showMenu()
+        })
+     }
+     else if(option === 2){
+        console.log("\n your Todo Lists");
+        todos.forEach((task,index) => {
+            console.log(`${index+1}.${task}`);
+        })
+        showMenu();
+        
+        }
+        else if(option === 3){
+            console.log('good bye');
+            rl.close();
+        }
+        else
+        {
+            console.log('invalid option .Please try again');
+            showMenu();
+        }
+     }
 
 showMenu();
