1190051bff17764af2631eaa35ea779f3c54cded
pritisharma1511 <ps0130815@gmail.com>
2025-09-29 22:52:53 +0530

    done

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -1,6 +1,11 @@
+import {readFile} from "fs/promises";
 import { createServer } from "http";
+import path from "path";
+
+const PORT = 3002;
 
 const server = createServer(async ( req,res) => {
+   console.log(req.url);
     if(req.method ==="GET") {
         if (req.url === "/"){
             try{
@@ -8,9 +13,24 @@ const server = createServer(async ( req,res) => {
                 res.writeHead(200,{"Content-Type" : "text/html"});
                 res.end(data);
           }catch (error){
-            res.writeHead(404,{"content=Type":"text/html"});
+            res.writeHead(404,{"Content-Type":"text/html"});
+            res.end("404 page not found")
+          }
+        }else if(req.method ==="GET") {
+        if (req.url === "/style.css"){
+            try{
+                const data = await readFile(path.join("public","style.css"));
+                res.writeHead(200,{"Content-Type" : "text/css"});
+                res.end(data);
+          }catch (error){
+            res.writeHead(404,{"Content-Type":"text/html"});
             res.end("404 page not found")
           }
         }
     }
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}
+  `);
 });
