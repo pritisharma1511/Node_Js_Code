16d5456cbfffc54005f18ffa7ecd5f1be0f69d54
pritisharma1511 <ps0130815@gmail.com>
2025-09-29 23:27:14 +0530

    done

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -3,33 +3,32 @@ import { createServer } from "http";
 import path from "path";
 
 const PORT = 3002;
+const serverFile = async (res,filePath,contentType) => {
+  try{
+        const data = await readFile(filePath);
+        res.writeHead(200,{"Content-Type" : contentType});
+        res.end(data);
+      }catch (error){
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end("404 page not found")
+      }
+}
 
 const server = createServer(async ( req,res) => {
    console.log(req.url);
     if(req.method ==="GET") {
         if (req.url === "/"){
-            try{
-                const data = await readFile(path.join("public","index.html"));
-                res.writeHead(200,{"Content-Type" : "text/html"});
-                res.end(data);
-          }catch (error){
-            res.writeHead(404,{"Content-Type":"text/html"});
-            res.end("404 page not found")
-          }
-        }else if(req.method ==="GET") {
-        if (req.url === "/style.css"){
-            try{
-                const data = await readFile(path.join("public","style.css"));
-                res.writeHead(200,{"Content-Type" : "text/css"});
-                res.end(data);
-          }catch (error){
-            res.writeHead(404,{"Content-Type":"text/html"});
-            res.end("404 page not found")
+            return serverFile(res, path.join("public","index.html"),"text/html");
+            
+        }else if(req.url === "/style.css"){
+            return serverFile(res, path.join("public","style.css"),"text/css");
           }
         }
-    }
-  }
-});
+
+
+
+        
+    });
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}
   `);
