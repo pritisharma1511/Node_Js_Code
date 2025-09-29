ace742201fb543f17f7539009333da9624aa8098
pritisharma1511 <ps0130815@gmail.com>
2025-09-29 19:06:39 +0530

    done

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -4,9 +4,13 @@ const server = createServer(async ( req,res) => {
     if(req.method ==="GET") {
         if (req.url === "/"){
             try{
+                const data = await readFile(path.join("public","index.html"));
+                res.writeHead(200,{"Content-Type" : "text/html"});
+                res.end(data);
           }catch (error){
             res.writeHead(404,{"content=Type":"text/html"});
+            res.end("404 page not found")
           }
         }
     }
-})
+});
