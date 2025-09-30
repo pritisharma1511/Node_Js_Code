903cd0da621dbb7c68fa0b559f3def933357c03a
pritisharma1511 <ps0130815@gmail.com>
2025-09-30 18:56:13 +0530

    done

--- express/app.js
+++ express/app.js
@@ -1,13 +1,16 @@
 import express from "express";
-import {PORT} from "./env.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res) => res.send("hello world !"));
-app.get("/about",(req,res) => res.send("hello about page !"));
+app.get("/", (req, res) => res.send("hello world !"));
+app.get("/about", (req, res) => res.send("hello about page !"));
 
-// const PORT = process.env.PORT || 3000;
-app.listen(PORT,() => {
-     console.log(`server is running at port : ${PORT}`);
+app.get("/product", (req, res) => {
+  console.log(req.query);
+  res.send(`<h1> user search for product ${req.query.search} mobile<h1>`);
+});
 
+app.listen(PORT, () => {
+  console.log(`server is running at port : ${PORT}`);
 });
--- express/env.js
+++ express/env.js
@@ -1,10 +1,10 @@
-// export const PORT = isNaN(process.env.PORT) ? 3000:
-// parseInt(process.env.PORT); 
+export const PORT = isNaN(process.env.PORT) ? 3000:
+parseInt(process.env.PORT); 
 
-import { z } from "zod";
+// import { z } from "zod";
 
-const ageSchema = z.number().min(18).max(100).int();
-const userAge = 17;
+// const ageSchema = z.number().min(18).max(100).int();
+// const userAge = 17;
 
-const parseUserAge = ageSchema.parse(userAge);
-console.log(parseUserAge);
+// const parseUserAge = ageSchema.parse(userAge);
+// console.log(parseUserAge);
