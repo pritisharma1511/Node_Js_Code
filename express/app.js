8c744c9946bec1c71f2b482fd17688cc465378dd
pritisharma1511 <ps0130815@gmail.com>
2025-09-30 13:09:52 +0530

    done

--- express/app.js
+++ express/app.js
@@ -3,6 +3,7 @@ import express from "express"
 const app = express();
 
 app.get("/",(req,res) => res.send("hello world !"));
+app.get("/about",(req,res) => res.send("hello about page !"));
 
 const PORT =3000;
 app.listen(PORT,() => {
