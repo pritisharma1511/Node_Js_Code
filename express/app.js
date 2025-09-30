b173402edf3c4aacb8e5af4a8e3f37f0ee87098f
pritisharma1511 <ps0130815@gmail.com>
2025-09-30 14:58:52 +0530

    ENV file done

--- express/app.js
+++ express/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/",(req,res) => res.send("hello world !"));
 app.get("/about",(req,res) => res.send("hello about page !"));
 
-const PORT =3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,() => {
      console.log(`server is running at port : ${PORT}`);
 
