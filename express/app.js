e86274d94856e291576e10ed33cafb31f0a6e270
pritisharma1511 <ps0130815@gmail.com>
2025-10-01 09:26:25 +0530

    done

--- express/app.js
+++ express/app.js
@@ -1,15 +1,32 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("hello world !"));
-app.get("/about", (req, res) => res.send("hello about page !"));
+// app.get("/", (req, res) => res.send("hello world !"));
+// app.get("/about", (req, res) => res.send("hello about page !"));
 
-app.get("/product", (req, res) => {
-  console.log(req.query);
-  res.send(`<h1> user search for product ${req.query.search} mobile<h1>`);
+// app.get("/product", (req, res) => {
+//   console.log(req.query);
+//   res.send(`<h1> user search for product ${req.query.search} mobile<h1>`);
+// });
+
+
+const staticPath = path.join(import.meta.dirname,"public");
+
+app.use(express.static(staticPath));
+
+app.use(express.urlencoded());
+
+app.post("/contact",(req,res) => {
+     console.log(req.body);
+     res.redirect("/");
 });
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`server is running at port : ${PORT}`);
