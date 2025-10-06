ccd246432d5cacf5d5d333ea2c35d78d67893e5f
pritisharma1511 <ps0130815@gmail.com>
2025-10-06 14:14:56 +0530

    mongodb server created and added

--- mongodb/mongo-driver.js
+++ mongodb/mongo-driver.js
@@ -1,1 +1,12 @@
-i
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://localhost");
+await client.connect();
+
+const db = client.db('mongo_nodejs_db');
+const userCollection = db.collection("users");
+
+userCollection.insertOne({name :"priti sharma",age : 20});
+
+
+
